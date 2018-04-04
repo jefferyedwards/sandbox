a25bccf764de53aaeee689d3433e3b56603d6231
Jeffery S Edwards <jeff@edwardsonthe.net>
2018-04-03 21:40:14 -0500

    update

--- k8s-l5d/hello-world/hello/src/main/java/net/edwardsonthe/Application.java
+++ k8s-l5d/hello-world/hello/src/main/java/net/edwardsonthe/Application.java
@@ -1,6 +1,10 @@
 package net.edwardsonthe;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import javax.annotation.PostConstruct;
+import javax.xml.bind.annotation.XmlRootElement;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +13,16 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
 @SpringBootApplication
 public class Application {
@@ -37,18 +46,60 @@ class Controller {
 
   RestTemplate restTemplate;
 
-  @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = "text/plain")
-  public String hello(@RequestParam(name = "source", defaultValue = "World") String source) {
+  @RequestMapping(value = "/hello", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Hello hello(@RequestParam(name = "source", defaultValue = "World") String source) throws UnknownHostException {
     LOGGER.info("~~~    uri = {}", worldUri + "/world?source=" + source);
-    String name = restTemplate.getForObject(worldUri + "/world?source=" + source, String.class);
-    LOGGER.info("Hello {}", name);
-    return "Hello" + name;
+    World world = restTemplate.getForObject(worldUri + "/world?source=" + source, World.class);
+    InetAddress inetAddress = InetAddress.getLocalHost();
+    Hello hello =
+        new Hello(inetAddress.getHostAddress(),
+                  inetAddress.getHostName(),
+                  System.getProperty("http.proxyHost"),
+                  System.getProperty("http.proxyPort"),
+                  world);
+    LOGGER.info("~~~ {}", hello);
+    return hello;
   }
 
   @PostConstruct
   void postConstruct() {
-    LOGGER.info("~~~~~ worldURI = {}", worldUri);
+    LOGGER.info("~~~ worldURI = {}", worldUri);
     restTemplate = builder.build();
   }
+
+}
+
+@AllArgsConstructor
+@Getter
+@ToString
+@XmlRootElement
+class Hello {
+
+  String addr;
+
+  String host;
+
+  String proxyHost;
+
+  String proxyPort;
+
+  World world;
+
+}
+
+@AllArgsConstructor
+@Getter
+@ToString
+class World {
+
+  String addr;
+
+  String host;
+
+  String proxyHost;
+
+  String proxyPort;
+
+  String source;
 
 }
--- k8s-l5d/hello-world/world/src/main/java/net/edwardsonthe/Application.java
+++ k8s-l5d/hello-world/world/src/main/java/net/edwardsonthe/Application.java
@@ -1,13 +1,21 @@
 package net.edwardsonthe;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 
 @SpringBootApplication
 public class Application {
@@ -23,10 +31,35 @@ class Controller {
 
   final Logger LOGGER = LoggerFactory.getLogger(Controller.class);
 
-  @RequestMapping(value = "/world", method = RequestMethod.GET, produces = "test/plain")
-  public String world(@RequestParam(name = "source", required = true) String source) {
+  @RequestMapping(value = "/world", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+  public World world(@RequestParam(name = "source", required = true) String source) throws UnknownHostException {
     LOGGER.info("~~~ source = {}", source);
-    return "... " + source;
+    InetAddress inetAddress = InetAddress.getLocalHost();
+    World world =
+        new World(inetAddress.getHostAddress(),
+                  inetAddress.getHostName(),
+                  System.getProperty("http.proxyHost"),
+                  System.getProperty("http.proxyPort"),
+                  source);
+    LOGGER.info("~~~ {}", world);
+    return world;
   }
+
+}
+
+@AllArgsConstructor
+@Getter
+@ToString
+class World {
+
+  String addr;
+
+  String host;
+
+  String proxyHost;
+
+  String proxyPort;
+
+  String source;
 
 }
